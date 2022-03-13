36e586f812a1e75118cee642b7c3d504c80d6f43
henriquebrito1 <henrrique_brito@hotmail.com>
2022-03-13 16:58:19 -0300

    Initial commit

--- src/br/com/dio/IMC.java
+++ src/br/com/dio/IMC.java
@@ -13,12 +13,33 @@ public class IMC {
         System.out.println("Informe sua idade");
         idade = scan.nextFloat();
         System.out.println("Informe seu peso");
-        peso=scan.nextFloat();
-        imc = peso/(altura*altura);
+        peso = scan.nextFloat();
+        imc = peso / (altura * altura);
         System.out.println("IMC" + imc);
+        {
+            if (imc < 18.5){
+            System.out.println(" Abaixo do peso");
+        }
+       else if (imc >18.5 && imc<=24.9)
+            {
+        System.out.println("Peso normal");
+    }
+           else if  (imc >25  && imc <= 29.9)
+            {
+            System.out.println("Sobrepeso ");
+           }
+           else if(imc >30  && imc <= 34.9)
+            {
+                System.out.println("Obesidade grau I");
+            }
+            else if(imc >35  && imc>= 39.9)
+            {
+                System.out.println("Obesidade grau II, Vá malhar pra não morrer ! ");
+            }
+
+
+        }
 
 
     }
-
-
 }
