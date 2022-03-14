a99421b136dd8fd0a9f7bf95c14d8efd3f6b3658
henriquebrito1 <henrrique_brito@hotmail.com>
2022-03-13 21:46:10 -0300

    Initial commit

---
+++ src/br/com/dio/Desafio1.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+import java.util.Scanner;
+public class Desafio1 {
+
+    public static void main(String[] args) {
+        int minutos;
+        Scanner input = new Scanner(System.in);
+        minutos = input.nextInt();
+        int K = (60* minutos   )/30;
+        System.out.println("%d minutos\n ,k" ); // Digite aqui o calculo dos minutos
+    }
+    int minutos =
+}
